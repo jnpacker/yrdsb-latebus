959a86ba2402b0dd16eacd30f2b76c082ad2ac1e
Ethan Packer <packer.ez@gmail.com>
2019-11-10 20:48:15 -0500

    use Panda

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CHECK_URL="http://net.schoolbuscity.com/latebus"
+export SCHOOL_NAME="Glenshields PS"
+export BUS_NUMBER="3116"
+export CHECK_DURATION=1
+python3 Latebus-limited-duration.py
